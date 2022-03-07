2532f3352f6dc4bf0e41ff486ef0659e6abcf160
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2022-03-07 13:56:04 +0600

    Tray icon

--- DriveSyncDesktop/Forms/MainForm.cs
+++ DriveSyncDesktop/Forms/MainForm.cs
@@ -6,19 +6,26 @@ namespace DriveSyncDesktop.Forms;
 public partial class MainForm : Form
 {
     private Process? _httpServerProcess;
+    private NotifyIcon _notifyIcon;
 
     public MainForm()
     {
         InitializeComponent();
+        _notifyIcon = new NotifyIcon();
     }
 
     private void backgroundWorkerForHttp_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
     {
         try
         {
+            // Run RClone Server with rclone.exe file
             toolStripStatusLabel.Text = "Starting RClone server";
             _httpServerProcess = RCloneService.RunHttpServer();
             toolStripStatusLabel.Text = "Successfully started RClone server";
+
+            // Set Notify Icon icon and set visibility true
+            _notifyIcon.Icon = new Icon("Resources/favicon.ico");
+            _notifyIcon.Visible = true;
         }
         catch (Exception ex)
         {
@@ -37,6 +44,8 @@ public partial class MainForm : Form
     }
     private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
     {
+        _notifyIcon.Dispose();
+
         if (!backgroundWorkerForHttp.WorkerSupportsCancellation) return;
 
         _httpServerProcess?.Kill();
